398b7e10d663cf19efece8e34ead7aa230f36c28
dowinterfor6 <dowinterfor6@gmail.com>
2019-12-21 16:16:21 -0800

    Added a class to handle formatting in the future, implemented basic display

--- dailyage.js
+++ dailyage.js
@@ -1,3 +1,5 @@
+const weeklies = require('./data/weeklies.json');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
@@ -9,8 +11,60 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.content === '!schedule') {
-      msg.reply('pong');
+      let weeklyDisplay = new Display();
+      let weeklyEventNames = Object.keys(weeklies);
+
+      weeklyEventNames.forEach(eventName => {
+        weeklyDisplay.add(weeklies[eventName]);
+      })
+
+      msg.reply(weeklyDisplay.print());
     }
 });
 
 client.login(auth.token);
+
+class Display {
+  constructor() {
+    this.state = {
+      "Mon": {},
+      "Tue": {},
+      "Wed": {},
+      "Thu": {},
+      "Fri": {},
+      "Sat": {},
+      "Sun": {},
+    }
+  }
+
+  add(event) {
+    if (event["Status"]) {
+      let eventName = event["Name"];
+      for (let [day, hours] of Object.entries(event["Times"])) {
+        for (let hour of hours) {
+          // console.log(eventName + "@ " + day + "@ " + hour);
+          if (Object.keys(this.state[day]).includes(hour)) {
+            this.state[day][hour].push(eventName);
+          } else {
+            this.state[day][hour] = [eventName];
+          }
+        }
+      }
+    }
+  }
+
+  print() {
+    let output = [];
+    for (let [day, schedule] of Object.entries(this.state)) {      
+      let scheduleDisplay = `\n${day}\`\`\``;
+      for (let [time, event] of Object.entries(schedule)) {
+        scheduleDisplay += time + ' ' + event.join(', ') + '\n';
+      }
+      scheduleDisplay += '```';
+
+      output.push(scheduleDisplay);
+    }
+
+    return output.join('');
+  }
+}
